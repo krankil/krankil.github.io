1b0d8c72974ea7d292b42883b41fcfbc56dbb003
Kirsi <95630527+krankil@users.noreply.github.com>
2023-04-09 14:42:33 +0300

    Update devops_04_08.js

--- devops_04_08.js
+++ devops_04_08.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 app.get("/", (req, res) => {
-    res.send('<p>Hello Rassu</p>');
+    res.send('<p>Hello Nero</p>');
 });
 app.listen(3000);
